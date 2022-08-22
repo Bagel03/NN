9cc9a378d4d48e33bade85a389fc1b50933d2651
Bagel03 <70449196+Bagel03@users.noreply.github.com>
2022-08-21 22:24:25 -0400

    Added RELU

--- src/activation.rs
+++ src/activation.rs
@@ -2,13 +2,24 @@ pub struct Activation {}
 
 // CHANGE THIS
 impl Activation {
-    // Sigmoid
-    pub fn function(value: f64) -> f64 {
-        1. / (1. + std::f64::consts::E.powf(-value))
-    }
+    /************************** SIGMOID *************************/
+    // pub fn function(value: f64) -> f64 {
+    //     1. / (1. + std::f64::consts::E.powf(-value))
+    // }
 
-    pub fn derivative(value: f64) -> f64 {
-        let activation = Activation::function(value);
-        activation * (1. - activation)
-    }
+    // pub fn derivative(value: f64) -> f64 {
+    //     let activation = Activation::function(value);
+    //     activation * (1. - activation)
+    // }
+
+    /************************** RELU *************************/
+    // pub fn function(value: f64) -> f64 {
+    //     if (value < 0 ) 0
+    //     value
+    // }
+
+    // pub fn derivative(value: f64) -> f64 {
+    //     if (value < 0 ) 0
+    //     1 
+    // }
 }
